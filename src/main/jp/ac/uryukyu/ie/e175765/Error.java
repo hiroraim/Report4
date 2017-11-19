bd0a12d703986ba0786b0886a45fa17a7a81e0f5
Hiroto Shimabukuro <e175765@ie.u-ryukyu.ac.jp>
2017-11-19 20:14:35 +0900

    first error

---
+++ src/main/jp/ac/uryukyu/ie/e175765/Error.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e175765;
+
+public class Error {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
