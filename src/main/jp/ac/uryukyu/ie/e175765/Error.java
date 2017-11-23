d61e0b86b61ee3848468918b9322b9c622b4932f
Hiroto Shimabukuro <e175765@ie.u-ryukyu.ac.jp>
2017-11-23 14:59:13 +0900

    secound error

--- src/main/jp/ac/uryukyu/ie/e175765/Error.java
+++ src/main/jp/ac/uryukyu/ie/e175765/Error.java
@@ -1,8 +1,14 @@
 package jp.ac.uryukyu.ie.e175765;
 
 public class Error {
-    public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args) {
+
+
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage()+"によるエラーが発生しました ");
+        }
     }
 }
