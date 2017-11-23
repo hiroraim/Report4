d38a3d81d65050a18eab697923c7516db5bc9e34
Hiroto Shimabukuro <e175765@ie.u-ryukyu.ac.jp>
2017-11-23 16:30:21 +0900

    third error

--- src/main/jp/ac/uryukyu/ie/e175765/Error.java
+++ src/main/jp/ac/uryukyu/ie/e175765/Error.java
@@ -3,12 +3,7 @@ package jp.ac.uryukyu.ie.e175765;
 public class Error {
     public static void main(String[] args) {
 
-
-        try {
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage()+"によるエラーが発生しました ");
-        }
+            String str = "3、14";
+        Double.parseDouble(str) ;
     }
 }
