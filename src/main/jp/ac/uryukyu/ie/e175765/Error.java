4a85d47a27a8593b904790ce30aeaaf0104a1e6c
Hiroto Shimabukuro <e175765@ie.u-ryukyu.ac.jp>
2017-11-24 11:25:04 +0900

    resecound error

--- src/main/jp/ac/uryukyu/ie/e175765/Error.java
+++ src/main/jp/ac/uryukyu/ie/e175765/Error.java
@@ -2,8 +2,14 @@ package jp.ac.uryukyu.ie.e175765;
 
 public class Error {
     public static void main(String[] args) {
+            try{
+                String str = null;
+                System.out.println(str.length());
+            } catch (NullPointerException e) {
+                System.out.println(e);
+                System.out.println(e.getMessage()+"によるエラーが発生しました ");
+            }
 
-            String str = "3、14";
-        Double.parseDouble(str) ;
+
     }
 }
